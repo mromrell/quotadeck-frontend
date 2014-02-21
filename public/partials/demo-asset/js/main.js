c6876e075786e31c72b7e64ad71a3fb8ea592ec6
Tyler Slater <tslater@goaptitude.com>
2014-02-20 23:10:04 -0700

    Landing Page creation

---
+++ public/partials/demo-asset/js/main.js
@@ -0,0 +1,20 @@
+   var dp = jQuery;
+   dp.noConflict();
+   dp(document).ready(function() {
+       dp('.ch-color li a').click(function(event) {
+           val = dp(this).attr('data-color');
+           dp("#clscheme").attr("href", "css/scheme/" + val + ".css");
+       });
+       dp('.cos-trigger').click(function(event) {
+           value = dp('.cos-wrapper').css('left') === '-180px' ? 0 : '-180px';
+           dp('.cos-wrapper').animate({
+               left: value,
+               duration: 750
+           });
+       });
+       dp('.ch-color li').click(function(event) {
+           dp('.ch-color li').removeClass('aktif');
+           dp(this).addClass('aktif');
+       });
+
+   });
